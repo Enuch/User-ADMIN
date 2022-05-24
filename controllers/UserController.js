267c35a6d6639cbae98b116c5923cef476d40ff5
Enuch <enuchsantys@gmail.com>
2022-05-24 15:10:11 -0300

    add func update users and users-admin, add id to the view user and admin

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -66,8 +66,11 @@ class UserController {
 
     addLine(dataUser) {
 
-        this.tableEl.innerHTML += `
-        <tr>
+        let tr = document.createElement('tr');
+
+        tr.dataset.user = JSON.stringify(dataUser);
+
+        tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
@@ -77,8 +80,31 @@ class UserController {
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-        </tr>
         `;
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+    }
+
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach( tr => {
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+            
+            if (user._admin) numberAdmin++;
+
+        });
+
+        document.querySelector('#number-users').innerHTML = numberUsers;
+        document.querySelector('#number-users-admin').innerHTML = numberAdmin;
+
     }
 
     getValues() {
